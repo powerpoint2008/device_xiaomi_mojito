b72b67dacfedfd268487903303a74e0d738dddd3
Sushrut1101 <guptasushrut@gmail.com>
2022-05-09 13:33:57 +0530

    mojito: Initial Bringup for fox_12.1
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -85,7 +85,6 @@ RECOVERY_BINARY_SOURCE_FILES += \
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libandroidicu \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
     libdisplayconfig.qti \
@@ -100,6 +99,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.qti.hardware.vibrator.impl.so \
     $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libqtivibratoreffect.so
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/mojito/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
