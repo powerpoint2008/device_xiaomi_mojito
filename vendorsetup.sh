b6afe1c26e13053e6fb133a8c84b54c3e7fcadaf
Sushrut1101 <guptasushrut@gmail.com>
2022-08-09 14:28:02 +0530

    mojito: Update the Build Vars
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,36 +32,44 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
+	export OF_USE_GREEN_LED=0
+	export FOX_ENABLE_APP_MANAGER=1
+	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
- 	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="mojito,sunny"
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export TARGET_DEVICE_ALT="sunny"
 	export OF_TARGET_DEVICES="mojito,sunny"
 	export OF_VIRTUAL_AB_DEVICE=1
-	export OF_AB_DEVICE=1
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_GREEN_LED=0
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
-
-	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
+	export OF_ENABLE_LPTOOLS=1
+	export FOX_USE_NANO_EDITOR=1
 	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
 	export FOX_DELETE_AROMAFM=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-	export FOX_ENABLE_APP_MANAGER=0
-	export FOX_DISABLE_APP_MANAGER=1
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
+
+	# Ensure that /sdcard is bind-unmounted before f2fs data repair or format
+	export OF_UNBIND_SDCARD_F2FS=1
+
+	# Instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
+        export OF_PATCH_VBMETA_FLAG="1"
+
+	# No special MIUI stuff
+        export OF_VANILLA_BUILD=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+
+	# Full Dynamic Partition size
+	export OF_DYNAMIC_FULL_SIZE=9126805504
 
 	# Screen Settings
 	export OF_SCREEN_H=2400
@@ -73,8 +81,21 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_ALLOW_DISABLE_NAVBAR=0
 	
 	# R11.1 Settings
-	export FOX_VERSION="R11.1"
+	export FOX_VERSION="R11.1_1"
+	export FOX_BUILD_TYPE="Beta"
 	export OF_MAINTAINER="Sushrut1101"
+
+	# Magisk
+	if [ -n "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
+		if [ ! -e "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
+			echo "Downloading the Latest Release of Magisk..."
+			LATEST_MAGISK_URL="$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep browser_download_url | grep Magisk- | cut -d : -f 2,3 | sed 's/"//g')"
+			mkdir -p $(dirname ${FOX_USE_SPECIFIC_MAGISK_ZIP})
+			wget ${LATEST_MAGISK_URL} -O ${FOX_USE_SPECIFIC_MAGISK_ZIP} 2>/dev/null
+			[ "$?" = "0" ] && echo "Magisk Downloaded Successfully"
+			echo "Done!"
+		fi
+	fi
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
